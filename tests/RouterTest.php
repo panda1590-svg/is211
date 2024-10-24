193e0f72bb1ffa4a3c230663acd5608f62d94535
Your Name <you@example.com>
2024-10-24 18:07:46 +0700

    bag

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -8,7 +8,19 @@ class RouterTest extends TestCase {
     public function test_router() {
         $router = new Router();
         $html = $router->route( "http://localhost/orders" );
-        $pos= mb_strpos($html, "Создание заказа");
+        $pos= mb_strpos($html, "С12121212оздание заказа");
+        $this->assertNotEquals(false, $pos);
+    }
+    public function test_router1() {
+        $router = new Router();
+        $html = $router->route( "http://localhost" );
+        $pos= mb_strpos($html, "Приглашаем в наш");
+        $this->assertNotEquals(false, $pos);
+    }
+    public function test_router2() {
+        $router = new Router();
+        $html = $router->route( "http://localhost/products" );
+        $pos= mb_strpos($html, "Добавить в корзину");
         $this->assertNotEquals(false, $pos);
     }
 }
